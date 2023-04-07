ef6f00d82c9049204939f36536b45a50ecfd8f2e
mjesteban <marjoes13@gmail.com>
2023-04-07 14:04:55 +0800

    Install DataTables

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 require('./bootstrap');
+import DataTable from 'datatables.net-bs5';
+import 'datatables.net-responsive-bs5';
+
+let table = new DataTable('#subscribers', {
+    responsive: true,
+    processing: true,
+    serverSide: false,
+});
