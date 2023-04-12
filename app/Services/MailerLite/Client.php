e63be74f46fb04962955b51cc6a64a6e18ed3fa8
mjesteban <marjoes13@gmail.com>
2023-04-12 20:13:53 +0800

    strip unused

--- app/Services/MailerLite/Client.php
+++ app/Services/MailerLite/Client.php
@@ -2,22 +2,6 @@
 
 namespace App\Services\MailerLite;
 
-use App\Services\MailerLite\Endpoints\Subscriber;
-
 class Client
 {
-    protected string $url = 'https://connect.mailerlite.com/';
-    protected string $contentType = 'application/json';
-    protected string $accept = 'application/json';
-
-    protected string $apiKey = '';
-    protected string $authorization = '';
-    protected int $rateLimit = 120;
-    protected string $httpMethod = 'get';
-    protected array $queryParams = [];
-
-    public function __construct()
-    {
-        $this->subscribers = new Subscriber($this);
-    }
 }
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -28,17 +28,11 @@
                     <a class="nav-link" href="{{ route('api-keys.create') }}">API Key</a>
                 </li>
             </ul>
-            <ul class="navbar-nav ms-md-auto">
-                <li class="nav-item">
-                    <a target="_blank" rel="noopener" class="nav-link" href="#"><i class="bi bi-linkedin"></i> LinkedIn</a>
-                </li>
-            </ul>
         </div>
     </div>
 </div>
     <div class="container">
         {{ $slot }}
     </div>
-{{--        Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})--}}
 </body>
 </html>
